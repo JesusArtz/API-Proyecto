262f0e42e326b400a314f27e62c1ce2795cd0053
Jesus Artz <realjesusartz@gmail.com>
2023-06-12 20:36:42 -0600

    Added a procfile

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ from src import ROOT
 for route in ROOT:
     app.add_url_rule(route['path'], view_func=route['function'], methods=[route['method']])
 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+
